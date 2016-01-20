a5b48f3ae53a65f1508aced716940cf062f4583b
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-01-20 18:17:12 -0500

    Introduction To Swift - [2] - Bronze Challenge

--- lessons/1_introduction_to_swift/variables/Sources/main.swift
+++ lessons/1_introduction_to_swift/variables/Sources/main.swift
@@ -1,11 +1,16 @@
+// Bronze Challenge: Introduction To Swift - Types, Constants and Variables
+
 // import Cocoa
 
+let townName: String = "Knowhere"
 let numberOfStoplights: Int = 4
+
+var levelOfUnemployment: Float
+levelOfUnemployment = 6.1
 
 var population: Int
 population = 5422
 
-let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(population) (where \(levelOfUnemployment)% are unemployed) and \(numberOfStoplights) stoplights."
 
-let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights."
 print(townDescription)
