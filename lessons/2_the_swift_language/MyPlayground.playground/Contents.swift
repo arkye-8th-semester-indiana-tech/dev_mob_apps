7c2c6e4983e999d35ee19ee768ab29b03b926fa6
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-02-01 13:41:22 -0500

    2. Literals and Subscripting

--- lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
+++ lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
@@ -6,9 +6,9 @@ var str = "Hello, playground"
 str = "Hello, Swift"
 let constStr = str
 
-var nextYear: Int
-var bodyTemp: Float
-var hasPet: Bool
-var arrayOfInts: [Int]
-var dictionaryOfCapitalsByCountry: [String:String]
-var winningLotteryNumbers: Set<Int>
+let number = 42
+let fmStation = 91.1
+
+let countingUp = ["one", "two"]
+let secondElement = countingUp[1]
+let nameByParkingSpace =  [13: "Alice", 27: "Bob"]
