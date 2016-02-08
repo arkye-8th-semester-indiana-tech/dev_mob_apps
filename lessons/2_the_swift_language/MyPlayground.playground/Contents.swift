b18fc7feed36ad676be226964cce4027a7f3dae4
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-02-08 11:57:43 -0500

    2. Properties

--- lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
+++ lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
@@ -2,21 +2,9 @@
 
 import Cocoa
 
-let emptyString = String()
-let emptyArrayOfInts = [Int]()
-let emptySetOfFloats = Set<Float>()
+let countingUp = ["one", "two"]
+let secondElement = countingUp[1]
+countingUp.count
 
-let defaultNumber = Int()
-let defaultBool = Bool()
-
-let number = 42
-let meaningOfLife = String(number)
-
-let availableRooms = Set([205, 411, 412])
-
-let defaultFloat = Float()
-let floatFromLiteral = Float(3.14)
-
-let easyPi = 3.14
-let floatFromDouble = Float(easyPi)
-let floatingPi: Float = 3.14
+let emptyString = ""
+emptyString.isEmpty
