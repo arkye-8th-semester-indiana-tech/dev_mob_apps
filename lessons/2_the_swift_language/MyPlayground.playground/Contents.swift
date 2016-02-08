3b7d25443e3e8ec764ef1bbcc03488d12ac77d09
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-02-08 12:19:06 -0500

    2. Loops and String Interpolation

--- lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
+++ lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
@@ -2,9 +2,30 @@
 
 import Cocoa
 
-let nameByParkingSpace = [13: "Alice", 27: "Bob"]
-if let space13Assignee = nameByParkingSpace[13] {
-    print("Key 13 is assigned in the dictionary!")
+var countingUp = ["one", "two"]
+countingUp.append("three")
+
+for var i = 0; i < countingUp.count; i++ {
+    let string = countingUp[i]
+    // Use 'string'
 }
 
-let space42Assignee: String? = nameByParkingSpace[42]
+let range = 0..<countingUp.count
+for i in range {
+    let string = countingUp[i]
+    // Use 'string'
+}
+
+for string in countingUp {
+    // Use 'string'
+}
+
+for (i, string) in countingUp.enumerate() {
+    // (0, "one"), (1, "two")
+}
+
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+
+for (space, name) in nameByParkingSpace {
+    let permit = "Space \(space): \(name)"
+}
