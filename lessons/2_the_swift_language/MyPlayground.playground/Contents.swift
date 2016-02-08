3eccb6007d834d561a192131a14dfa6b28d0cc1d
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-02-08 12:06:29 -0500

    2. Optionals

--- lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
+++ lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
@@ -12,6 +12,12 @@ var reading3: Float?
 
 reading1 = 9.8
 reading2 = 9.2
-// reading3 = 9.7
-let avgReading = (reading1! + reading2! + reading3!)/3
+reading3 = 9.7
 
+if let r1 = reading1,
+    r2 = reading2,
+    r3 = reading3 {
+        let avgReading = (r1 + r2 + r3) / 3
+} else {
+    let errorString = "Instrument reported a reading that was a nil."
+}
