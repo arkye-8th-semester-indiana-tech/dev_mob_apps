90a7bd39a83b46b4229b13bc49adfa9c82d9f848
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-02-08 12:09:00 -0500

    2. Subscripting Dictionaries

--- lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
+++ lessons/2_the_swift_language/MyPlayground.playground/Contents.swift
@@ -2,22 +2,9 @@
 
 import Cocoa
 
-var anOptionalFloat: Float?
-var anOptionalArrayOfStrings: [String]?
-var anOptionalArrayOfOptionalStrings: [String?]?
-
-var reading1: Float?
-var reading2: Float?
-var reading3: Float?
-
-reading1 = 9.8
-reading2 = 9.2
-reading3 = 9.7
-
-if let r1 = reading1,
-    r2 = reading2,
-    r3 = reading3 {
-        let avgReading = (r1 + r2 + r3) / 3
-} else {
-    let errorString = "Instrument reported a reading that was a nil."
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+if let space13Assignee = nameByParkingSpace[13] {
+    print("Key 13 is assigned in the dictionary!")
 }
+
+let space42Assignee: String? = nameByParkingSpace[42]
