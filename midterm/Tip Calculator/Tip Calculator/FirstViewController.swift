3ba21f9d029a59df5c4c51f426fbd8b5c2e480f5
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-02-29 13:28:55 -0500

    Create XCode Project

---
+++ midterm/Tip Calculator/Tip Calculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tip Calculator
+//
+//  Created by Maia de Moraes, Jonathan H - 01 on 2/29/16.
+//  Copyright © 2016 Jonathan Henrique Maia de Moraes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
