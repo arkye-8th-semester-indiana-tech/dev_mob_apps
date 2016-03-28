13e69963fdce8a8ec22100e2355718a9f34e68ed
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-03-28 13:12:46 -0400

    9. UITableView's Data Source

---
+++ lessons/9_UITableView_and_UITableViewController/Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,22 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Maia de Moraes, Jonathan H - 01 on 3/28/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+}
