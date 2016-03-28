6dbbeb316831409fc2cca44fcec4a67853662004
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-03-28 12:20:18 -0400

    9. Subclassing UITableViewController

---
+++ lessons/9_UITableView_and_UITableViewController/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Maia de Moraes, Jonathan H - 01 on 3/28/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
