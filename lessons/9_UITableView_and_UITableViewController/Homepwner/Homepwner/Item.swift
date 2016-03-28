77ddc4112bb8034302a70142018763b718507694
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-03-28 12:23:08 -0400

    9. Creating the Item Class

---
+++ lessons/9_UITableView_and_UITableViewController/Homepwner/Homepwner/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Maia de Moraes, Jonathan H - 01 on 3/28/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: NSDate
+}
