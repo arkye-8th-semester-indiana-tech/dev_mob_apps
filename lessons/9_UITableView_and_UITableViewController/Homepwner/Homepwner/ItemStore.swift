e3097e66671de370d8e1af837c40e244fa1cf81d
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-03-28 13:18:22 -0400

    9. Giving The Controller Access To The Store

--- lessons/9_UITableView_and_UITableViewController/Homepwner/Homepwner/ItemStore.swift
+++ lessons/9_UITableView_and_UITableViewController/Homepwner/Homepwner/ItemStore.swift
@@ -11,6 +11,12 @@ import UIKit
 class ItemStore {
     
     var allItems = [Item]()
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
 
     func createItem() -> Item {
         let newItem = Item(random: true)
