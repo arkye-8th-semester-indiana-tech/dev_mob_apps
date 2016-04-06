f1e0ace5515027d7416e3d2127247e1b58c765b5
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2016-04-06 13:27:51 -0400

    11. Exposing the Properties of ItemCell

---
+++ lessons/11_Subclassing_UITableViewCell/Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Maia de Moraes, Jonathan H - 01 on 4/6/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
